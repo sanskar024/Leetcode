dd619c346993d57baf771b3abb4670b7ff8f5ca1
sanskar gupta <sanskargupta008009@gmail.com>
2026-09-15 23:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.87%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-bool used[10]={false};
+
 void solve(int idx,int k,int n, vector<vector<int>>&ans, vector<int>&curr){
     if(k==0){
         if(n==0){
@@ -10,12 +10,10 @@ void solve(int idx,int k,int n, vector<vector<int>>&ans, vector<int>&curr){
     }
    
     for(int i=idx;i<=9;i++){
-        if(used[i])continue;
         curr.push_back(i);
-    used[i]=true;
         solve(i+1,k-1,n-i,ans,curr);
         curr.pop_back();
-        used[i]=false;
+    
     }
 }
     vector<vector<int>> combinationSum3(int k, int n) {
